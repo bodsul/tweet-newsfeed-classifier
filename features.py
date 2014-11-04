685db365e45b7fcb3c07cd15fd76c561104a4966
bodsul <bodsul@gmail.com>
2014-11-04 09:53:59 -0600

    Delete features.py

--- features.py
+++
@@ -1,8 +0,0 @@
-#returns dictionary containing features of tweets given relevant words
-def get_features(tweet, rel_words):
-    tweet_words = tweet.split()
-    features = {}
-    for word in rel_words:
-        features['has(%s)' % word] = (word in tweet_words)
-    return features
-    
